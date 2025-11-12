aa8ffcf4e45dec260213376db75ca04baa9cad70
SarthakKanade <sarthakkanade527@gmail.com>
2025-11-12 14:29:21 +0530

    Initial FSM and Animation Done

---
+++ Assets/Scripts/PlayerStates/Player_DashState.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Player_DashState : PlayerState
+{
+    private float originalGravityScale;
+    private int dashDirection;
+
+    public Player_DashState(Player player, StateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        stateTimer = player.dashDuration;
+
+        if (player.moveInput.x != 0)
+        {
+            dashDirection = (int)player.moveInput.x;
+        }
+        else
+        {
+            dashDirection = player.facingDirection;
+        }
+
+        originalGravityScale = rb.gravityScale;
+        rb.gravityScale = 0;
+    }
+
+
+    public override void Update()
+    {
+        base.Update();
+        CancelDashIfNeeded();
+        player.SetVelocity(player.dashSpeed * dashDirection, 0);
+
+
+        if (stateTimer < 0)
+        {
+            if (player.groundDetected)
+                stateMachine.ChangeState(player.idleState);
+            else
+                stateMachine.ChangeState(player.fallState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        player.SetVelocity(0, 0);
+        rb.gravityScale = originalGravityScale;
+    }
+
+    private void CancelDashIfNeeded()
+    {
+        if (player.wallDetected)
+        {
+            if (player.groundDetected)
+                stateMachine.ChangeState(player.idleState);
+            else
+                stateMachine.ChangeState(player.wallSlideState);
+        }
+    }
+}
