2c1f24c9cc25020825eb29d67fee45665e59fb10
SarthakKanade <sarthakkanade527@gmail.com>
2025-12-20 00:29:24 +0530

    Sword Skill Object

--- Assets/Scripts/SkillSystem/SkillObject_Base.cs
+++ Assets/Scripts/SkillSystem/SkillObject_Base.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class SkillObject_Base : MonoBehaviour
 {
+    [SerializeField] private GameObject OnHitVFX;
+    [Space]
     [SerializeField] protected LayerMask whatIsEnemy;
     [SerializeField] protected Transform targetCheck;
     [SerializeField] protected float checkRadius = 1;
@@ -9,6 +11,7 @@ public class SkillObject_Base : MonoBehaviour
     protected Entity_Stats playerStats;
     protected DamageScaleData damageScaleData;
     protected ElementType usedElement;
+    protected bool targetGotHit;
 
     protected void DamageEnemiesInRadius(Transform t, float radius)
     {
@@ -27,11 +30,13 @@ public class SkillObject_Base : MonoBehaviour
             ElementType element = attackData.element;
 
 
-            damgable.TakeDamage(physDamage, elemDamage, element, transform);
+            targetGotHit = damgable.TakeDamage(physDamage, elemDamage, element, transform);
 
             if (element != ElementType.None)
                 statusHandler?.ApplyStatusEffect(element, attackData.effectData);
 
+            if(targetGotHit)
+                Instantiate(OnHitVFX, target.transform.position, Quaternion.identity);
 
             usedElement = element;
         }
